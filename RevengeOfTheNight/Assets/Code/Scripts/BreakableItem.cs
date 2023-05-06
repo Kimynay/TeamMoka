49b41cab41f04305ff64eb91512273b4d67d80cc
Kimynay <tom.pois@hotmail.com>
2023-05-06 14:49:56 +0200

    broken variants

--- RevengeOfTheNight/Assets/Code/Scripts/BreakableItem.cs
+++ RevengeOfTheNight/Assets/Code/Scripts/BreakableItem.cs
@@ -13,9 +13,15 @@ public class BreakableItem : MonoBehaviour
     public float stabilisationSpeed = 10;
     private GameObject image;
     private float localTime = 0.0f;
+    public bool disapearWhenBreak = true;
+    public bool throwPartWhenBraek = true;
+    public bool changeImageWhenBreak = false;
 
+    private bool broken = false;
+    private bool haveFell = false;
     public BreakingType BreakingMode = 0;
     public enum BreakingType { Fall, Scratch, ScratchLoosePart}
+    public List<GameObject> brokenParts;
     void Start()
     {
         image = transform.GetChild(0).gameObject;
@@ -50,21 +56,24 @@ public class BreakableItem : MonoBehaviour
     {
         if (BreakingMode == BreakingType.Fall)
         {
-            if (currentForce > 1.0f)
+            if (!haveFell)
             {
-                GetComponent<BoxCollider2D>().enabled = false;
-                Debug.Log("Falliiiiiiiing");
-                Fall();
-            }
-            else
-            {
-                image.transform.rotation = Quaternion.Euler(0, 0, (currentForce * maxAngle) * Mathf.Sin(localTime * (shakingMaxSpeed * Mathf.Abs(currentForce - 1.0f))));
-                currentForce -= Time.deltaTime / stabilisationSpeed;
-                if (currentForce <= 0.01f)
+                if (currentForce > 1.0f)
                 {
-                    currentForce = 0.0f;
-                    image.transform.rotation = Quaternion.Euler(0, 0, 0);
-                    isShaking = false;
+                    Debug.Log("reee");
+                    GetComponent<BoxCollider2D>().enabled = false;
+                    Fall();
+                }
+                else
+                {
+                    image.transform.rotation = Quaternion.Euler(0, 0, (currentForce * maxAngle) * Mathf.Sin(localTime * (shakingMaxSpeed * Mathf.Abs(currentForce - 1.0f))));
+                    currentForce -= Time.deltaTime / stabilisationSpeed;
+                    if (currentForce <= 0.01f)
+                    {
+                        currentForce = 0.0f;
+                        image.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        isShaking = false;
+                    }
                 }
             }
         }
@@ -78,10 +87,32 @@ public class BreakableItem : MonoBehaviour
     void Fall()
     {
         image.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        haveFell = true;
     }
 
     public void Break()
     {
-        Debug.Log("Break");
+        if (!broken)
+        {
+            if (disapearWhenBreak)
+            {
+                image.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+            }
+            if (changeImageWhenBreak && image.transform.GetChild(1) != null)
+            {
+                image.transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = true;
+            }
+            if (throwPartWhenBraek)
+            {
+                for (int i = 0; i < brokenParts.Count; i++)
+                {
+                    GameObject part = Instantiate(brokenParts[i], image.transform.position + Vector3.up * 1f, Quaternion.identity);
+                    float rand = Random.Range(-1f, 1f);
+                    Vector2 dir = new Vector2(rand, 1f).normalized;
+                    part.GetComponent<Rigidbody2D>().AddForce(dir * 3f, ForceMode2D.Impulse);
+                }
+                broken = true;
+            }
+        }
     }
 }
